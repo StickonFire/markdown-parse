675d680813aeadc3153b3bb68b3cb2fb2f651c15
StickonFire <70039286+StickonFire@users.noreply.github.com>
2022-02-03 11:26:08 -0800

    Updated MarkdownTest to have failcase
    JUnit version 4.10
    ...23
    E
    Time: 0.032
    There was 1 failure:
    1) testSecondaryBug(MarkdownParseTest)
    java.lang.AssertionError: expected:<[www.weirdP(]> but was:<[www.weirdP().com]>

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,6 +1,11 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
+import java.util.ArrayList;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class MarkdownParseTest {
     @Test
     public void addition() {
@@ -10,5 +15,13 @@ public class MarkdownParseTest {
     @Test
     public void subtraction() {
         assertEquals(8, 9-1);
+    }
+
+    @Test
+    public void testSecondaryBug() throws IOException{
+        ArrayList<String> links = new ArrayList<String>();
+        links.add("www.weirdP().com");
+        String issue = Files.readString(Path.of("edge-case.md"));
+        assertEquals(MarkdownParse.getLinks(issue),links);
     }
 }

4ab0cbfcd1350b008a45c5fdaa632de06425fcd3
StickonFire <70039286+StickonFire@users.noreply.github.com>
2022-02-25 14:35:56 -0800

    Added Tests for LabReport4

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -37,4 +37,31 @@ public class MarkdownParseTest {
         String[] links = {"google.com"};
         autoTestMarkdownParse(links, "edge_case2.md");
     }
+
+    /**
+     * Tests getLinks with snippet1.md
+     */
+    @Test
+    public void testSnippet1(){
+        List<String> snippet1Results = List.of("`google.com","google.com","ucsd.edu");
+        assertEquals(snippet1Results,MarkdownParse.getLinks("snippet1.md"));
+    }
+
+    /**
+     * Tests getLinks with snippet2.md
+     */
+    @Test
+    public void testSnippet2(){
+        List<String> snippet2Results = List.of("a.com","a.com(())","example.com");
+        assertEquals(snippet2Results,MarkdownParse.getLinks("snippet2.md"));
+    }
+
+    /**
+     * Tests getLinks with snippet3.md
+     */
+    @Test
+    public void testSnippet3(){
+        List<String> snippet3Results = List.of("https://ucsd-cse15l-w22.github.io/");
+        assertEquals(snippet3Results,MarkdownParse.getLinks("snippet3.md"));
+    }
 }
